a5d804b975d9aee82060aedaf811be43c75285bd
winfuture <insoul_net@qq.com>
2019-10-23 01:13:27 +0800

    完成使用pageable page进行分页

--- src/test/java/com/xywei/test/EmployeeDaoTest.java
+++ src/test/java/com/xywei/test/EmployeeDaoTest.java
@@ -4,6 +4,9 @@ import com.xywei.dao.EmployeeDao;
 import com.xywei.domain.Employee;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -78,5 +81,42 @@ public class EmployeeDaoTest extends SpringTest {
     @Test
     public void testFindBySQL() {
         System.out.println(employeeDao.findBySQL("Python"));
+    }
+
+    /**
+     * @Description 分页，证明了分页是一部分一部分查询出来的
+     * @Author future
+     * @DateTime 2019/10/23 0:43
+     **/
+    @Test
+    public void testPage() {
+        System.out.println("1-----------------------------------------------------------------------------------");
+        int currentPage = 0;
+        int pageSize = 3;
+        Pageable pageable = new PageRequest(currentPage, pageSize);
+        Page<Employee> page = employeeDao.findAll(pageable);
+        System.out.println("第" + (currentPage + 1) + "页数据总数：>>>>>>>>>>>>>>>>>>>" + page.getTotalElements());
+        System.out.println("第" + (currentPage + 1) + "页所有数据内容：>>>>>>>>>>>>>>>>>>>" + page.getContent());
+        System.out.println("总共页数：>>>>>>>>>>>>>>>>>>>>>>>>>" + page.getTotalPages());
+
+        System.out.println("2-----------------------------------------------------------------------------------");
+
+        currentPage = 1;
+        pageSize = 3;
+        pageable = new PageRequest(currentPage, pageSize);
+        page = employeeDao.findAll(pageable);
+        System.out.println("第" + (currentPage + 1) + "页数据总数：>>>>>>>>>>>>>>>>>>>" + page.getTotalElements());
+        System.out.println("第" + (currentPage + 1) + "页所有数据内容：>>>>>>>>>>>>>>>>>>>" + page.getContent());
+        System.out.println("总共页数：>>>>>>>>>>>>>>>>>>>>>>>>>" + page.getTotalPages());
+        pageable = new PageRequest(currentPage, pageSize);
+
+        System.out.println("3-----------------------------------------------------------------------------------");
+        pageable = page.nextPageable();
+        page = employeeDao.findAll(pageable);
+        System.out.println("第" + (pageable.getPageNumber() + 1) + "页数据总数：>>>>>>>>>>>>>>>>>>>" + page.getTotalElements());
+        System.out.println("第" + (pageable.getPageNumber() + 1) + "页所有数据内容：>>>>>>>>>>>>>>>>>>>" + page.getContent());
+        System.out.println("总共页数：>>>>>>>>>>>>>>>>>>>>>>>>>" + page.getTotalPages());
+
+
     }
 }
